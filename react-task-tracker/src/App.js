114e6e305623692e2070f0abe283eddd762e4eda
Abir Bourbia <ja_bourbia@esi.dz>
2023-03-26 23:22:05 +0100

    i added the reminder feature statically

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -27,14 +27,25 @@ function App() {
     ]
 )
 
+
+// delete function
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// toggle reminder
+const toggleReminder = (id) =>{
+  setTasks(tasks.map((task)=> task.id===id ? 
+  {...task, reminder:!task.reminder}: task))
 }
   return (
     <div className="container">
       <Header/>
-      {tasks.length>0 ? <Tasks tasks={tasks} 
-      onDelete={deleteTask}/> : 'No Tasksto show'}
+      { tasks.length>0 ?
+      (<Tasks tasks={tasks} 
+      onDelete={deleteTask} onToggle={toggleReminder}/>) : (
+        'No Tasks to Show'
+        )}
     </div>
   );
 }
